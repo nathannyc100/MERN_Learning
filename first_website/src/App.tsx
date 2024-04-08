83049f0fbc7042e14c45895076ab306cfbce4d8f
Nathan Yu <nathannycnathannyc@gmail.com>
2024-04-08 18:29:45 +0800

    refactor: changed most files to 4 space indent

--- first_website/src/App.tsx
+++ first_website/src/App.tsx
@@ -4,66 +4,68 @@ import React from 'react'
 function App() {
 
 
-  return (
-    <>
-    <Board />
+    return (
+            <>
+            <Board />
 
-    </>
-  )
+            </>
+           )
 
 }
 
 interface SquareVars {
-  value: number;
+value: number;
 }
 
 interface BoardVars {
-  value: number;
+value: number;
 }
 
 class Square extends React.Component<SquareVars> {
-  handleClick() {
-    console.log("square clicked");
-  };
+    handleClick() {
+        console.log("square clicked");
+    };
 
-  render() {
-    return (
-      <button className = "square">
-        {this.props.value}
-      </button>
-    );
-  }
+    render() {
+        return (
+                <button className = "square">
+                {this.props.value}
+                </button>
+               );
+    }
 }
 
 class Board extends React.Component<{}, BoardVars> {
-  renderSquare(i: number) {
-    return <Square value = {i} />;
-  }
+    renderSquare(i: number) {
+        return <Square value = {i} />;
+    }
 
-  render() {
-    const status: string = "hello"
-    return (
-      <div>
-        <div className='status'>{status}</div>
-        <div className='board-row'>
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className='board-row'>
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className='board-row'>
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    )
-  }
+
+    render() {
+        const status: string = "hello"
+            return (
+                    <div>
+                    <div className='status'>{status}</div>
+                    <div className='board-row'>
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                    </div>
+                    <div className='board-row'>
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                    </div>
+                    <div className='board-row'>
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                    </div>
+                    </div>
+                   )
+    }
 }
+
 
 
 export default App
