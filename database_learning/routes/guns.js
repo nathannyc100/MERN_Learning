1608cf5745163d095cc44c80ea106b1b321659f3
nathannyc100 <107283236+nathannyc100@users.noreply.github.com>
2024-06-27 10:46:39 +0800

    feat: add gun routing

--- database_learning/routes/guns.js
+++ database_learning/routes/guns.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import Gun from '../models/gunModel'
 
 const router = express.Router()
 
@@ -10,8 +11,16 @@ router.get('/:id', (req, res) => {
     res.json({mssg: 'get specific gun'})
 })
 
-router.post('/', (req, res) => {
-    res.json({mssg: 'add new gun'})
+router.post('/', async (req, res) => {
+    const {name} = req.body
+
+
+    try {
+        const gun = await Gun.create()
+        res.status(200).json(gun)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 })
 
 router.delete('/:id', (req, res) => {
