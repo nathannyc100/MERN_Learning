d0ca15101f9156810b5b92d5697be0a2ac93ca0c
nathannyc100 <107283236+nathannyc100@users.noreply.github.com>
2024-06-28 22:36:20 +0800

    feat: added schema timestamps

--- database_learning/models/childModel.js
+++ database_learning/models/childModel.js
@@ -4,6 +4,6 @@ const { Schema } = mongoose;
 
 const childSchema = new Schema({
     name: {type: String, required: true},
-});
+}, { timestamps: true });
 
 export default mongoose.model('Child', childSchema);
--- database_learning/models/parentModel.js
+++ database_learning/models/parentModel.js
@@ -6,7 +6,7 @@ const { Schema } = mongoose;
 const parentSchema = new Schema({
     name: {type: String, required: true},
     child: {type: Schema.Types.ObjectId, ref: 'Child'},
-});
+}, { timestamps: true });
 
 
 export default mongoose.model('Parent', parentSchema);
